981883f780052e2d3e7fc5992a951890dfd09450
TurnipXenon <turnipxenon@gmail.com>
2022-09-12 18:15:07 -0600

    Improve get.py

--- lab01/get.py
+++ lab01/get.py
@@ -1,6 +1,15 @@
 import requests
 
-content = requests.get("https://raw.githubusercontent.com/TurnipXenon/CMPUT404Lab/main/lab01/get.py").content
-with open("source_code.txt", "w") as f:
-    print(content, file=f)
-print(content)
+def get_source():
+    response = requests.get("https://raw.githubusercontent.com/TurnipXenon/CMPUT404Lab/main/lab01/get.py")
+    
+    if response.status_code != 200 and response.status_code != 201:
+        print(f"Error: status code: {response.status_code}")
+        return
+        
+    content = response.content.decode("utf-8")
+    with open("source_code.txt", "w") as f:
+        print(content, file=f)
+    print(content)
+
+get_source()
